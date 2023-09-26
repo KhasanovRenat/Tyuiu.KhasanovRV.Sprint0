7a5b52ac53d21da5ea01c4787e927475110ba7df
Ренат Хасанов <Tyuiu.KhasanovRV@outlook.com>
2023-09-26 18:59:00 +0500

    Task5

---
+++ Tyuiu.KhasanovRV.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KhasanovRV.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KhasanovRV.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a3c7f29c-0649-4ac6-b8ad-018ebd91d578")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
